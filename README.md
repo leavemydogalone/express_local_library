f4aa787eadc1ca57a1a816af330844b71284ed49
Leavemydogalone <niklasport88@gmail.com>
2021-03-17 10:22:54 -0700

    deployed on heroku. link in readme

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 A library app following the MDN tutorial. 90% of the code was copy/pasted while the code for updating and deleting certain objects was written by me.
 
 Heorku link for deployed app:
+https://pure-shore-76092.herokuapp.com/catalog
--- app.js
+++ app.js
@@ -7,8 +7,12 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var catalogRouter = require('./routes/catalog'); //Import routes for "catalog" area of site
+var compression = require('compression');
+var hemlet = require('helmet');
 
 var app = express();
+
+app.use(hemlet());
 
 var mongoose = require('mongoose');
 var dev_db_url =
@@ -26,6 +30,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(compression());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
